4d61f4425833d6a14346822fabcd980b6151c5ed
nimyears <45189482+jienian@users.noreply.github.com>
2023-05-02 14:20:29 +0800

    链表求和

--- Linked/链表求和.java
+++ Linked/链表求和.java
@@ -43,3 +43,45 @@ if(fort > 0){
     ListNode node = new ListNode(1);
     pre.next = node;    
 }
+class Solution{
+public static void main(String[] args) {
+    ListNode l1 = new ListNode(2);
+    l1.next = new ListNode(4);
+    l1.next.next = new ListNode(3);
+
+    ListNode l2 = new ListNode(5);
+    l2.next = new ListNode(6);
+    l2.next.next = new ListNode(4);
+
+    Solution solution = new Solution();
+    ListNode result = solution.addTwoNumbers(l1, l2);
+
+    while(result != null){
+        System.out.print(result.val + " ");
+        result = result.next;
+    }
+}
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int fort = 0;
+        ListNode head = new ListNode(0);
+        ListNode pre = head;
+        while(l1 != null || l2 != null){
+            int s1 = l1 != null ? l1.val : 0;
+            int s2 = l2 != null ? l2.val : 0;
+            int all = s1 + s2 + fort; 
+            fort = all / 10;
+            ListNode node = new ListNode(all % 10);
+            pre.next = node;
+            pre = pre.next;
+            l1 = l1 != null ? l1.next : null;
+            l2 = l2 != null ? l2.next : null;
+        }
+        if(fort > 0){
+            ListNode node = new ListNode(1);
+            pre.next = node;
+        }
+        return head.next;
+    }
+}
+    

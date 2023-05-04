de3b8006b7bf2ef79c998df352703eed1a82d832
nimyears <45189482+jienian@users.noreply.github.com>
2023-05-04 13:10:34 +0800

    改检查子树.java

--- Grach and Tree/检查子树.java
+++ Grach and Tree/检查子树.java
@@ -9,7 +9,7 @@
  */
 class Solution {
     public boolean isSame(TreeNode t1, TreeNode t2) {
-        if(t1 == null || t2 == null) return true;   
+        if(t1 == null && t2 == null) return true;   
         if(t1 == null || t2 == null) return false;
             return t1.val == t2.val && isSame(t1.left, t2.left) && isSame(t1.right, t2.right);
     }

b3874e2e3a9f5340f6054a0986d96fe9dd4a55d9
nimyears <45189482+jienian@users.noreply.github.com>
2023-05-04 13:05:08 +0800

    检查子树.java

---
+++ Grach and Tree/检查子树.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSame(TreeNode t1, TreeNode t2) {
+        if(t1 == null || t2 == null) return true;   
+        if(t1 == null || t2 == null) return false;
+            return t1.val == t2.val && isSame(t1.left, t2.left) && isSame(t1.right, t2.right);
+    }
+
+    public boolean checkSubTree(TreeNode t1, TreeNode t2){
+        if(t1 == null) return t2 == null;
+        return isSame(t1, t2)|| checkSubTree(t1.left,t2) || checkSubTree(t1.right, t2);
+    }
+
+}

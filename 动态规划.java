b3a9c3fa4a43090c27d22f4e697ee817e67ae7ff
nimyears <45189482+jienian@users.noreply.github.com>
2023-05-06 13:02:56 +0800

    魔术索引

--- 动态规划.java
+++ 动态规划.java
@@ -44,4 +44,12 @@ class Solution {
         return dp[(n - 4) % 3];
     }
 }
-//
+//魔术索引
+
+public int findMagicIndex(int[] nums){
+    for(int i = 0; i < nums.length; i++){
+        if(i == nums[i]) return i;
+
+    }
+    return -1;
+}

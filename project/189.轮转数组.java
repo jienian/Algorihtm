e10839247ae4d240ef091a67405baded80eeaf14
nimyears <45189482+jienian@users.noreply.github.com>
2023-05-08 13:50:17 +0800

    轮转数组

---
+++ project/189.轮转数组.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        int[] newArr  = new int[n];
+        for(int i = 0; i < n; ++i){
+            newArr[(i + k) % n] = nums[i];
+         }
+         System.arraycopy(newArr, 0, nums, 0, n);
+    }
+}

3c7396e9a8eec5a1bf83a8fe51adf34d53b9b9f8
nimyears <45189482+jienian@users.noreply.github.com>
2023-05-09 13:36:36 +0800

    检查子树解法二

--- Grach and Tree/检查子树.java
+++ Grach and Tree/检查子树.java
@@ -20,3 +20,39 @@ class Solution {
     }
 
 }
+
+
+//解法二:
+class Solution {
+  /**
+   * 检查 t2 是否是 t1 的子树
+   *
+   * @param t1 树 t1
+   * @param t2 树 t2
+   * @return t2 是否是 t1 的子树
+   */
+  public boolean checkSubTree(TreeNode t1, TreeNode t2) {
+    StringBuilder string1 = new StringBuilder();
+    StringBuilder string2 = new StringBuilder();
+    getOrderString(t1, string1);
+    getOrderString(t2, string2);
+    return string1.indexOf(string2.toString()) != -1;
+  }
+
+  /**
+   * 按前序遍历顺序获取二叉树的字符串表示
+   *
+   * @param node 当前节点
+   * @param sb   用于构建字符串的 StringBuilder
+   */
+  public void getOrderString(TreeNode node, StringBuilder sb) {
+    if (node == null) {
+      sb.append("X"); // 表示空节点
+      return;
+    }
+    sb.append(node.val + " "); // 将节点的值加入字符串
+    getOrderString(node.left, sb); // 递归处理左子树
+    getOrderString(node.right, sb); // 递归处理右子树
+  }
+}
+

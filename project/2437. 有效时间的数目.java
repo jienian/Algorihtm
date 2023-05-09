c662c2e79b2661f00d8bc928a6b4079d0b6d956b
nimyears <45189482+jienian@users.noreply.github.com>
2023-05-09 14:13:34 +0800

    格式

--- project/2437. 有效时间的数目.java
+++ project/2437. 有效时间的数目.java
@@ -19,8 +19,8 @@ class Solution {
     private int f(String s, int m) {
         int cnt = 0;
         for (int i = 0; i < m; ++i) {
-            boolean a = s.charAt(0) == '?' || s.charAt(0) - '0' == i / 10;
-            boolean b = s.charAt(1) == '?' || s.charAt(1) - '0' == i % 10;
+            boolean a = s.charAt(0) == '?' || (s.charAt(0) - '0' == i / 10);
+            boolean b = s.charAt(1) == '?' || (s.charAt(1) - '0' == i % 10);
             cnt += a && b ? 1 : 0;
         }
         return cnt;

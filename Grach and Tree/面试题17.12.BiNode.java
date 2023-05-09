2fde5476cd5e2e586b206a63e96130ab00f9ecef
nimyears <45189482+jienian@users.noreply.github.com>
2023-05-09 17:11:51 +0800

    BiNode

---
+++ Grach and Tree/面试题17.12.BiNode.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    TreeNode head = new TreeNode(-1);   // 为了返回单向链表的头节点而多设的一个节点
+    TreeNode perv = null;               // 指向当前节点的前一个节点
+    public TreeNode convertBiNode(TreeNode root) {
+        helper(root);
+        return head.right;
+    }
+
+    public void helper(TreeNode root) {
+        if (root == null) { return;}
+        helper(root.left);
+        if (perv == null) {     // 第一个节点
+            perv = root;        // 记录第一个节点
+            head.right = root;  // 记录它，它将作为单链表的表头
+        } else {                // 第一个节点之后的节点
+            perv.right = root;  // 前一个节点的右指针指向当前节点
+            perv = root;        // 更新perv指向
+        }
+        root.left = null;       // 当前节点的左指针设为null
+        helper(root.right);
+    }
+}

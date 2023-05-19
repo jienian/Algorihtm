4dc91c0bcdd246fdb3b46d7e3960e5bb44efe09c
nimyears <45189482+jienian@users.noreply.github.com>
2023-05-20 00:24:32 +0800

    168

---
+++ project/168. Excel表列名称.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        StringBuffer sb = new StringBuffer();
+        while (columnNumber != 0) {
+            columnNumber--;
+            sb.append((char)(columnNumber % 26 + 'A'));
+            columnNumber /= 26;
+        }
+        return sb.reverse().toString();
+    }
+}

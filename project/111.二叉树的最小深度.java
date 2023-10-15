1755e23d1a27443b9cad0636db5037d1ea8247c7
Nimyears <45189482+jienian@users.noreply.github.com>
2023-10-15 07:44:01 +0000

    二叉树的最小深度

---
+++ project/111.二叉树的最小深度.java
@@ -0,0 +1,16 @@
+/**
+ * 111.二叉树的最小深度
+ */
+public class Solution {
+
+    public int minDepth(TreeNode root ){
+        if(root == null) return 0;
+        if(root.left == null && root.right == null) return 1;
+        int m1 = minDepth(root.left);
+        int m2 = minDepth(root.right);
+        if(root.left == null || root.right == null) return m1 + m2 + 1;
+        return Math.min(m1, m2) + 1;
+    }
+
+    
+}

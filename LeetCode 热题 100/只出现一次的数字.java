2976c60e8095b364d52586b50639a31640397460
jienian <2129300193@qq.com>
2023-11-07 17:14:58 +0800

    补充单元测试

--- LeetCode 热题 100/只出现一次的数字.java
+++ LeetCode 热题 100/只出现一次的数字.java
@@ -1,11 +1,19 @@
 //O(n)
 //O(1)
 class Solution {
-    public int singleNumber(int[] nums) {
+    public static int singleNumber(int[] nums) {
         int single = 0;
         for(int num : nums) {
-            single ^= num;
+            single ^= num; // 使用异或运算符对每个元素进行异或操作
         }
         return single;
     }
+
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 1, 3, 2, 3};
+        int result = singleNumber(nums);
+        System.out.println(result);
+    }
+
 }

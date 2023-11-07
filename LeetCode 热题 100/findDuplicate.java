39fe283975b4285125c6f811022da327af52ea3a
jienian <2129300193@qq.com>
2023-11-07 19:17:55 +0800

    287. 寻找重复数

---
+++ LeetCode 热题 100/findDuplicate.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow = 0, fast = 0;
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while (slow != fast);
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,3,4,2,2};
+        int result = findDuplicate(nums);
+        System.out.println(result);
+    }
+}

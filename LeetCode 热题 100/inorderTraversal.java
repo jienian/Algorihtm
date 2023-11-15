2850824ffcd7785f8b328259ff99fecd3fd02f06
jienian <2129300193@qq.com>
2023-11-15 18:45:01 +0800

    二叉树的中序遍历

---
+++ LeetCode 热题 100/inorderTraversal.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+import javax.swing.tree.TreeNode;
+
+class Solution{
+  public List<Integer> inorderTraversal(TreeNode root){
+    List<Integer> res = new ArrayList<Integer>();
+    inorder(root,res);
+    return res;
+  }
+  public void inorder(TreeNode root, List<Integer> res){
+    if(root == null){
+      return;
+    }
+    inorder(root.left,res);
+    res.add(root.val);
+    inorder(root.right,res);
+  }
+}

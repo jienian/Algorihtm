89db585629ec5b6a2a0700a479fce629a64554db
Nimyears <45189482+jienian@users.noreply.github.com>
2024-04-23 17:32:10 +0800

    Create searchRange.java

---
+++ project/searchRange.java
@@ -0,0 +1,28 @@
+/*
+  1.Input: nums = [5,7,7,8,8,10], target = 8
+  Output: [3,4]
+  2.Input: nums = [5,7,7,8,8,10], target = 6
+  Output: [-1,-1]
+*/
+public int[] searchRange(int[] nums, int target) {
+    int first = findFirst(nums, target);
+    int last = findFirst(nums, target + 1) - 1;
+    if (first == nums.length || nums[first] != target) {
+        return new int[]{-1, -1};
+    } else {
+        return new int[]{first, Math.max(first, last)};
+    }
+}
+
+private int findFirst(int[] nums, int target) {
+    int l = 0, h = nums.length; // 注意 h 的初始值
+    while (l < h) {
+        int m = l + (h - l) / 2;
+        if (nums[m] >= target) {
+            h = m;
+        } else {
+            l = m + 1;
+        }
+    }
+    return l;
+}

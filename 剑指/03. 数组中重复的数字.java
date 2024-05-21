4bc797fe6b633d06a84b256180cf29dbb696c2d6
Nimyears <45189482+jienian@users.noreply.github.com>
2024-05-21 01:13:28 +0000

    03

---
+++ 剑指/03. 数组中重复的数字.java
@@ -0,0 +1,29 @@
+//容易想到使用哈希表（Set）记录数组的各个数字，当查找到重复数字则直接返回。
+class Solution {
+    public int findRepeatNumber(int[] nums) {
+        Set<Integer> dic = new HashSet<>();
+        for(int num : nums) {
+            if(dic.contains(num)) return num;
+            dic.add(num);
+        }
+        return -1;
+    }
+}
+
+//第二种方法：原地交换
+class Solution {
+    public int findRepeatNumber(int[] nums) {
+        int i = 0;
+        while(i < nums.length) {
+            if(nums[i] == i) {
+                i++;
+                continue;
+            }
+            if(nums[nums[i]] == nums[i]) return nums[i];
+            int tmp = nums[i];
+            nums[i] = nums[tmp];
+            nums[tmp] = tmp;
+        }
+        return -1;
+    }
+}

cf81614326a4b8029ac49beeb76980cf7aa0e8da
Nimyears <45189482+jienian@users.noreply.github.com>
2024-05-21 10:53:57 +0000

    剑指

---
+++ 剑指/04.findNumberIn2DArray.java
@@ -0,0 +1,13 @@
+//剑指 Offer 04. 二维数组中的查找.md O(m+n)，O(1)
+class Solution {
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        int i = matrix.length - 1, j = 0;
+        while(i >= 0 && j < matrix[0].length)
+        {
+            if(matrix[i][j] > target) i--;
+            else if(matrix[i][j] < target) j++;
+            else return true;
+        }
+        return false;
+    }
+}
---
+++ 剑指/05.replaceSpace.java
@@ -0,0 +1,12 @@
+//05. 替换空格
+class Solution {
+    public String replaceSpace(String s) {
+        StringBuilder res = new StringBuilder();
+        for(Character c : s.toCharArray())
+        {
+            if(c == ' ') res.append("%20");
+            else res.append(c);
+        }
+        return res.toString();
+    }
+}

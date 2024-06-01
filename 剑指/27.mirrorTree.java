e602281db3690d30cf907920623d3baa5757d508
Nimyears <45189482+jienian@users.noreply.github.com>
2024-06-01 03:15:44 +0000

    mirrorTree

---
+++ 剑指/27.mirrorTree.java
@@ -0,0 +1,9 @@
+class Solution {
+    public TreeNode mirrorTree(TreeNode root) {
+        if(root == null) return null;
+        TreeNode tmp = root.left;
+        root.left = mirrorTree(root.right);
+        root.right = mirrorTree(tmp);
+        return root;
+    }
+}

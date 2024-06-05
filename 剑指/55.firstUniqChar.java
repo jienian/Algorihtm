8e5f56c19d94d5f1db452303e85aef0403756b13
Nimyears <45189482+jienian@users.noreply.github.com>
2024-06-05 06:40:39 +0000

    firstUniqChar

---
+++ 剑指/55.firstUniqChar.java
@@ -0,0 +1,12 @@
+//剑指 Offer 50. 第一个只出现一次的字符.md
+class Solution {
+    public char firstUniqChar(String s) {
+        HashMap<Character, Boolean> dic = new HashMap<>();
+        char[] sc = s.toCharArray();
+        for(char c : sc)
+            dic.put(c, !dic.containsKey(c));
+        for(char c : sc)
+            if(dic.get(c)) return c;
+        return ' ';
+    }
+}

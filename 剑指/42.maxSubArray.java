027e8b2e4c3f0c9f9eb702043167be87cde65d55
Nimyears <45189482+jienian@users.noreply.github.com>
2024-06-05 06:41:41 +0000

    maxSubArray

---
+++ 剑指/42.maxSubArray.java
@@ -0,0 +1,11 @@
+//剑指 Offer 42. 连续子数组的最大和.md
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int res = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            nums[i] += Math.max(nums[i - 1], 0);
+            res = Math.max(res, nums[i]);
+        }
+        return res;
+    }
+}

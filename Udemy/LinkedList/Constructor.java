ff57621a8474ab02b965dfe4cca2fc5ab2cf798f
Nimyears <45189482+jienian@users.noreply.github.com>
2024-06-16 09:16:15 +0000

    Constructor

---
+++ Udemy/LinkedList/Constructor.java
@@ -0,0 +1,85 @@
+```java
+public class LinkedList {
+    
+    private Node head;
+    private Node tail;
+    private int length;
+     
+    class Node {
+        int value;
+        Node next;
+     
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+
+    //   +===================================================+
+    //   |                  WRITE YOUR CODE HERE             |
+    //   | Description:                                      |
+    //   | - Constructor for the LinkedList class.           |
+    //   | - Initializes the linked list with a single node. |
+    //   |                                                   |
+    //   | Parameters:                                       |
+    //   | - value: The integer value of the first node in   |
+    //   |   the newly created linked list.                  |
+    //   |                                                   |
+    //   | Behavior:                                         |
+    //   | - A new Node is created with the given value.     |
+    //   | - This node is set as both the head and tail of   |
+    //   |   the list, indicating it is the only node in the |
+    //   |   list at creation.                               |
+    //   | - The length of the list is initialized to 1.     |
+    //   +===================================================+
+
+   // 构造方法，用于初始化链表
+    public LinkedList(int value) {
+        Node newNode = new Node(value); // 创建一个新的节点
+        head = newNode; // 将新节点设置为头部
+        tail = newNode; // 将新节点设置为尾部
+        length = 1; // 链表的长度初始化为1
+    }
+    
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public int getLength() {
+        return length;
+    }
+    
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+    
+    public void printAll() {
+        if (length == 0) {
+            System.out.println("Head: null");
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Head: " + head.value);
+            System.out.println("Tail: " + tail.value);
+        }
+        System.out.println("Length:" + length);
+        System.out.println("\nLinked List:");
+        if (length == 0) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+    }
+
+}
+
+
+```

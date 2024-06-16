9dc120f7879cda5b7c51eeea8b4d0524be55add6
Nimyears <45189482+jienian@users.noreply.github.com>
2024-06-16 14:44:53 +0000

    Append

---
+++ Udemy/LinkedList/Append.java
@@ -0,0 +1,99 @@
+public class LinkedList {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (length == 0) {
+            System.out.println("Head: null");
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Head: " + head.value);
+            System.out.println("Tail: " + tail.value);
+        }
+        System.out.println("Length:" + length);
+        System.out.println("\nLinked List:");
+        if (length == 0) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+    }
+    
+    public void makeEmpty() {
+        head = null;
+        tail = null;
+        length = 0;
+    }
+
+	// WRITE APPEND METHOD HERE //
+	//                          //
+	//                          //
+	//                          //
+	//                          //
+	//////////////////////////////
+
+     // 添加节点到链表的尾部
+     public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) { // 如果链表为空
+            head = newNode;
+            tail = newNode;
+        } else { // 链表不为空
+            tail.next = newNode; // 将新节点添加到尾部
+            tail = newNode; // 更新尾节点
+        }
+        length++; // 链表长度增加
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if(length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        length++;
+    }
+
+}
+

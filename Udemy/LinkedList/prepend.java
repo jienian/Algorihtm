17ec8c24d1ff0dc843b27974cdb3c0460c646aa9
Nimyears <45189482+jienian@users.noreply.github.com>
2024-06-17 01:27:08 +0000

    Prepend

---
+++ Udemy/LinkedList/prepend.java
@@ -0,0 +1,117 @@
+public class LinkedList {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (length == 0) {
+            System.out.println("Head: null");
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Head: " + head.value);
+            System.out.println("Tail: " + tail.value);
+        }
+        System.out.println("Length:" + length);
+        System.out.println("\nLinked List:");
+        if (length == 0) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+    }
+    
+    public void makeEmpty() {
+        head = null;
+        tail = null;
+        length = 0;
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        length++;
+    }
+
+    public Node removeLast() {
+        if (length == 0) return null;
+        Node temp = head;
+        Node pre = head;
+        while(temp.next != null) {
+            pre = temp;
+            temp = temp.next;
+        }
+        tail = pre;
+        tail.next = null;
+        length--;
+        if (length == 0) {
+            head = null;
+            tail = null;
+        }
+        return temp;
+    }
+
+	// WRITE PREPEND METHOD HERE //
+	//                           //
+	//                           //
+	//                           //
+	//                           //
+	///////////////////////////////
+    public void prepend(int value) {
+        Node newNode = new Node(value); // 创建一个新节点
+        if (length == 0) {
+            // 如果链表为空，新节点即为头节点也为尾节点
+            head = newNode;
+            tail = newNode;
+        } else {
+            // 如果链表不为空，将新节点的next指向当前的头节点
+            newNode.next = head;
+            // 更新头节点为新节点
+            head = newNode;
+        }
+        length++; // 链表长度增加1
+    }
+}
+    

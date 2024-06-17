3dd102be861385b0634e294b21e005e74b5d1afe
Nimyears <45189482+jienian@users.noreply.github.com>
2024-06-17 02:59:50 +0000

    findMideeleNode的Main函数

--- Udemy/LinkedList/findMiddleNode.java
+++ Udemy/LinkedList/findMiddleNode.java
@@ -8,6 +8,44 @@ public Node findMiddleNode() {
 		slow = slow.next;
 		fast = fast.next.next;
 	}
-    
+
 	return slow;
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.append(2);
+        myLinkedList.append(3);
+        myLinkedList.append(4);
+        myLinkedList.append(5);
+        
+
+        System.out.println("1 -> 2 -> 3 -> 4 -> 5");
+        System.out.println( "Middle Node: "+ myLinkedList.findMiddleNode().value);
+        
+        
+        myLinkedList.append(6);
+        
+        
+        System.out.println("===========================");
+        System.out.println("1 -> 2 -> 3 -> 4 -> 5 -> 6");
+        System.out.println( "Middle Node: "+ myLinkedList.findMiddleNode().value);
+
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            1 -> 2 -> 3 -> 4 -> 5
+            Middle Node: 3
+            ===========================
+            1 -> 2 -> 3 -> 4 -> 5 -> 6
+            Middle Node: 4
+
+        */
+
+    }
+
 }
